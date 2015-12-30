b66d5e8af46da256030d0a0245f2d58aee435a64
Padmate <959853840@qq.com>
2015-12-30 13:21:57 +0800

    Test Mike repo

---
+++ src/DpControl/Domain/Entities/Alarm.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DpControl.Domain.Entities
+{
+    public class Alarm
+    {
+        public int AlarmId { get; set; }
+
+        #region relationship
+        public int AlarmMessageId { get; set; }
+        public AlarmMessage AlarmMessage { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+        #endregion
+        public DateTime ModifiedDate { get; set; }
+        public byte[] RowVersion { get; set; }
+
+    }
+}
