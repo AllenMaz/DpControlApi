9e590f75b5b05895d00cd4773fd421c0e6d44428
Padmate <959853840@qq.com>
2015-12-30 12:50:09 +0800

    Branch修改

--- src/DpControl/Controllers/Test/ValuesController.cs
+++ src/DpControl/Controllers/Test/ValuesController.cs
@@ -9,7 +9,10 @@ namespace DpControl.Controllers.Test
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET: api/values
+        /// <summary>
+        /// 获取所有
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<string> Get()
         {
