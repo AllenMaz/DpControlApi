f5a0f87d8600f6fdae5ed11014949624f438beb5
Padmate <959853840@qq.com>
2016-01-28 16:09:46 +0800

    Add UnitTests

---
+++ DpControl.UnitTest/UtilityTests/ResponseHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using DpControl.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DpControl.UnitTest.UtilityTests
+{
+    public class ResponseHandlerTest
+    {
+        [Theory]
+        [InlineData(new object[] { 404, "Cound not found" })]
+        public void TestReturnError(int httpStatusCode,string message)
+        {
+            string errorJson = ResponseHandler.ReturnError(httpStatusCode,message);
+            Assert.IsType(typeof(string), errorJson);
+        }
+    }
+}
