49597b0b2351354f446e0f812bbd07a57b9adb76
Padmate <959853840@qq.com>
2016-03-01 15:28:48 +0800

    update

--- src/DpControl/Startup.cs
+++ src/DpControl/Startup.cs
@@ -34,7 +34,9 @@ namespace DpControl
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
+                //when publish ,filename mu be certain name
+                //.AddJsonFile($"appsettings.Development.json", optional: true);
+            
             if (env.IsEnvironment("Development"))
             {
                 // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
