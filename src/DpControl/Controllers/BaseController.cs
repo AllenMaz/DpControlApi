3e1dccd010f5966cff2b1ac868ae2c0b6772f300
Padmate <959853840@qq.com>
2016-03-15 15:18:01 +0800

    BaseController

---
+++ src/DpControl/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using DpControl.Models;
+using DpControl.Utility;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DpControl.Controllers
+{
+    public class BaseController : Controller
+    {
+        public TableParams GetJqueryTableParams()
+        {
+           
+            HttpRequest rq = Request;
+            StreamReader srRequest = new StreamReader(rq.Body);
+            String strReqStream = srRequest.ReadToEnd();
+            List<JQueryTableParam> lsParams = JsonHandler.UnJson<List<JQueryTableParam>>(strReqStream);
+
+            TableParams tableParames = new TableParams();
+            if (lsParams.Count >0)
+            {
+                tableParames.sEcho = (int)lsParams.Where(v => v.name == "sEcho").First().value;
+                tableParames.iDisplayStart = (int)lsParams.Where(v => v.name == "iDisplayStart").First().value;
+                tableParames.iDisplayLength = (int)lsParams.Where(v => v.name == "iDisplayLength").First().value;
+
+
+            }
+            return tableParames;
+        }
+    }
+}
